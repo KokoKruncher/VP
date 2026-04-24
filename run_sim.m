ba544a1c7dc5ed6d5ec74e388177b8278d00df35
Aiman Danish <aimandanish5308@gmail.com>
2026-04-24 23:59:15 +0100

    Added backtracking for multiple iterations

--- run_sim.m
+++ run_sim.m
@@ -34,9 +34,21 @@ mu_lat              = 1.36;   % Tyre friction coefficient [-], Cornering
 % Radius_corner       = [50 15 35 70];        % Radius of each corner [m]
 % Angle_corner        = [90 151 38 81];       % Angle of each corner [deg]
 % Length_straight     = [742 405 368 53];     % Length of each straight [m]
-Radius_corner       = [50 15 35 140];        % Radius of each corner [m]
-Angle_corner        = [90 151 38 81];       % Angle of each corner [deg]
-Length_straight     = [742 405 368 53];     % Length of each straight [m]
+
+% Accel too short
+% Radius_corner       = [50 15 35 140];        % Radius of each corner [m]
+% Angle_corner        = [90 151 38 81];       % Angle of each corner [deg]
+% Length_straight     = [742 405 368 53];     % Length of each straight [m]
+
+% Brake too short
+% Radius_corner       = [100 15 35 70];        % Radius of each corner [m]
+% Angle_corner        = [45 151 38 81];       % Angle of each corner [deg]
+% Length_straight     = [742 20 368 53];     % Length of each straight [m]
+
+% Brake too short more than once
+Radius_corner       = [100 15 35 200];        % Radius of each corner [m]
+Angle_corner        = [45 151 38 40];       % Angle of each corner [deg]
+Length_straight     = [5 20 368 53];     % Length of each straight [m]
 
 %Simulation parameters
 Delta_S             = 0.1;   % Calculation step size interval [m]
@@ -71,7 +83,7 @@ vehicle.MMotorMapLookup         = Motor_torque_lookup(2,:);
 vehicle.tire = tire;
 
 %% Run the lap
-steadyStateSim = SteadyStateLapSimulation(track, vehicle, distanceStep=Delta_S);
+steadyStateSim = SteadyStateLapSimulation(track, vehicle, distanceStep=Delta_S, enableLoggingToCommandWindow=true);
 lap = steadyStateSim.run();
 
 %% Print outputs
@@ -116,12 +128,12 @@ end
 end
 
 %% Plot tire friction
-figure('Name', 'Tire Friction Plot');
-plot(lap.results.sRun, lap.results.muDynamicF, 'Color', '#f2b248', 'LineWidth', 1.5, 'DisplayName', 'Front');
-hold on;
-plot(lap.results.sRun, lap.results.muDynamicR, 'Color', '#7c4081', 'LineWidth', 1.5, 'DisplayName', 'Rear');
-grid on;
-xlabel('Distance (m)')
-ylabel('Friction coefficient');
-title('Dynamic tire friction');
-legend();
+% figure('Name', 'Tire Friction Plot');
+% plot(lap.results.sRun, lap.results.muDynamicF, 'Color', '#f2b248', 'LineWidth', 1.5, 'DisplayName', 'Front');
+% hold on;
+% plot(lap.results.sRun, lap.results.muDynamicR, 'Color', '#7c4081', 'LineWidth', 1.5, 'DisplayName', 'Rear');
+% grid on;
+% xlabel('Distance (m)')
+% ylabel('Friction coefficient');
+% title('Dynamic tire friction');
+% legend();
